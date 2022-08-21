8cbbeb436b6fdec19ccab9d0c4f1f5c402650889
Dean Marchiori <deanmarchiori@gmail.com>
2022-08-21 16:11:20 +1000

    clean up

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
-#' Summary of SMPD4-Related Clinical Phenotype
+#' SMPD4-Related Clinical Phenotype Data Set
 #'
-#' A dataset from Table S2 of Magini, Pamela, et al. "Loss of SMPD4 causes a 
+#' A dataset of clical features and phenotypes from Table S2 of Magini, Pamela, et al. "Loss of SMPD4 causes a 
 #' developmental disorder characterized by microcephaly and congenital 
 #' arthrogryposis." The American Journal of Human 
 #' Genetics 105.4 (2019): 689-705.  
@@ -8,6 +8,10 @@
 #' This data has been transformed into an analysis-ready tidy data set. 
 #'
 #' @format A tibble: 21 x 152
+#' 
+#' \describe{
+#'   \item{_ind}{columns ending in _ind are indicator columns created by mergine duplicate tokenised terms into one feature.}
+#' }
 #' 
 #' @source \url{https://doi.org/10.1016/j.ajhg.2019.08.006}
 "smpd4_phenotype"
